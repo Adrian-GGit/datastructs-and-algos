ad0cc74f21b3bee3d4a3a04af08332a727634731
Adrian Junge <adjun@web.de>
2023-12-30 09:48:35 -0500

    impl: merge sort, quick sort, tests

---
+++ algorithms/sorting/merge_sort.py
@@ -0,0 +1,34 @@
+from typing import Generic, TypeVar, Callable
+from operator import lt
+
+T = TypeVar('T')
+
+class MergeSort(Generic[T]):
+    def __init__(self, lst: list[T], comp_func: Callable[[T, T], bool] = lt) -> None:
+        self.comp_func = comp_func
+        self.lst = lst
+
+
+    def sort(self, lst: list[T] = None) -> list[T]:
+        if lst is None:
+            lst = self.lst
+
+        if len(lst) <= 1:
+            return lst
+        else:
+            len_lst_half = len(lst) // 2
+            left, right = lst[:len_lst_half], lst[len_lst_half:len(lst)]
+            left = self.sort(left)
+            right = self.sort(right)
+            self.lst = self.merge(left, right)
+            return self.lst
+
+
+    def merge(self, left: list[T], right: list[T]) -> list[T]:
+        sorted_lst = []
+        while left and right:
+            next_item = left.pop(0) if self.comp_func(left[0], right[0]) else right.pop(0)
+            sorted_lst.append(next_item)
+        sorted_lst.extend(left)
+        sorted_lst.extend(right)
+        return sorted_lst
