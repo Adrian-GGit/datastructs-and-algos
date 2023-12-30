27a885735c32b16f85fd90d1de96fa272b8e5d92
Lukas Petermann <lukas.petermann13@gmail.com>
2023-12-30 12:53:57 +0100

    fix backtracking, more tests, more debugging

--- src/DFS/dfs.py
+++ src/DFS/dfs.py
@@ -1,3 +1,7 @@
+from copy import copy
+
+def map_to_char(l: list[int]) -> list[str]:
+        return [chr(x+97) if x >= 0 else "-" for x in l]
 
 class DFS:
 
@@ -7,23 +11,37 @@ class DFS:
         self.reset()
         self.verbose = verbose
 
-    def annotate(func: callable):
+    def print_members(self):
+        print("\toReps:       ", map_to_char(self.open_reps))
+        print("\toNodes:      ", map_to_char(self.open_nodes))
+        print("\tcomponent[w]:", map_to_char(self.component))
+        
+    def debug(func: callable):
         def inner(self, *args):
-            if self.verbose:
-                print(f"{func.__name__}{args}")
             func(self, *args)
+            self.steps += 1
+            if self.verbose:
+                converted_args = tuple(map(lambda x: chr(x+97), args))
+                print(f"Step {self.steps}: {func.__name__}{converted_args}:")
+                self.print_members()
+                print("-----------")
+            if self.steps == self.stop_after:
+                self.intermediate_result = (copy(self.open_nodes), copy(self.open_reps), copy(self.component))
         return inner
 
     def reset(self):
         self.dfs_pos = 1
         self.dfs_num = [-1] * len(self.V)
-        self.component = [v for v in self.V]     # list of corr. component for each node
-        self.visited = set()                    # "marked" nodes
-        self.open_nodes = []                    # stack of all nodes in open sccs
-        self.open_reps = []                     # stack of all representatives of open sccs
+        self.component = [-1] * len(self.V)            # list of corr. component for each node. Per default, each node is its own component
+        self.visited = set()                     # "marked" nodes
+        self.open_nodes = []                     # stack of all nodes in open sccs
+        self.open_reps = []                      # stack of all representatives of open sccs
         self.finished = False
+        self.steps = 0
+        self.stop_after = None
 
-    def dfs(self):
+    def process(self, yield_after_step=-1):
+        self.stop_after = yield_after_step
         for v in self.V:
             if v in self.visited:
                 continue
@@ -43,27 +61,27 @@ class DFS:
                 self._dfs(v, w)
         self._backtrack(u, v)
 
-    @annotate
+    @debug
     def _root(self, node: int):
         self.open_reps.append(node)
         self.open_nodes.append(node)
         self.dfs_num[node] = self.dfs_pos
         self.dfs_pos += 1
 
-    @annotate
+    @debug
     def _traverse_non_tree_edge(self, v: int, w: int):
         if w in self.open_nodes:
             while self.dfs_num[w] < self.dfs_num[self.open_reps[-1]]:
                 self.open_reps.pop()
 
-    @annotate  
+    @debug  
     def _traverse_tree_edge(self, v: int, w: int):
         self.open_reps.append(w)
         self.open_nodes.append(w)
         self.dfs_num[w] = self.dfs_pos
         self.dfs_pos += 1
 
-    @annotate
+    @debug
     def _backtrack(self, u: int, v: int):
         if len(self.open_reps) == 0:
             return
@@ -71,6 +89,6 @@ class DFS:
             self.open_reps.pop()
             while True:
                 w = self.open_nodes.pop()
+                self.component[w] = v
                 if w == v:
                     break
-                self.component[w] = v
--- tests/test_dfs.py
+++ tests/test_dfs.py
@@ -1,17 +1,33 @@
-from fixtures import list_graph, backtrack_graph
+from tests.fixtures.dfsFixtures import list_graph, backtrack_graph, tutorial_graph
 from src.DFS import dfs
 
+VERBOSE = True
 
-def test_pos_increment_after(list_graph):
+def test_num_sccs_listCase(list_graph):
     instance = dfs.DFS(list_graph)
-    instance.dfs()
+    instance.process()
     num_closed_scc = len(set(instance.component))
-
     assert num_closed_scc == len(list_graph)
     
     
-def test_pos_increment_after_bt(backtrack_graph):
-    instance = dfs.DFS(backtrack_graph)
-    instance.dfs()
+def test_num_sccs_lectureCase(backtrack_graph):
+    instance = dfs.DFS(backtrack_graph, verbose=VERBOSE)
+    instance.process()
+    print(instance.component)
     num_closed_scc = len(set(instance.component))
     assert num_closed_scc == 4
+    
+def test_scc_members_tutorialCase(tutorial_graph):
+    instance = dfs.DFS(tutorial_graph, verbose=VERBOSE)
+    instance.process()
+    sol_component = ['a', 'b', 'b', 'b', 'b', 'a', 'a', 'h']
+
+    assert sol_component == dfs.map_to_char(instance.component)      
+    
+def test_intermediate_tutorialCase(tutorial_graph):
+    instance = dfs.DFS(tutorial_graph, verbose=VERBOSE)
+    instance.process(yield_after_step=12)
+    _, _, components = instance.intermediate_result
+    sol_component = ['-', 'b', 'b', 'b', 'b', '-', '-', '-']
+    assert sol_component == dfs.map_to_char(components)      
+    
